4d36ec8fc17c65bd676e26c0f81db9c6c576ce52
Aaron Watters <awatters@flatironinstitute.org>
2025-10-24 15:32:02 -0400

    expose npyjs

--- src/index.ts
+++ src/index.ts
@@ -5,3 +5,6 @@ export const name = 'CanvasVolumeTS';
 // export all modules from the src directory
 export * as display from './display';
 export * as volume from './volume';
+
+// also export npyjs for convenience
+export * as npyjs from 'npyjs';
